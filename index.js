ec9f85fc0e8624b5a84c4f3af4417cd7c95b955b
adityagawade27 <aditya.gawade@colorado.edu>
2021-01-26 20:46:24 -0800

    finished...with some weird working

--- index.js
+++ index.js
@@ -27,8 +27,52 @@ const server = http.createServer((req, res) => {
    // Build File Path 
    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 
-   console.log(filePath);
-   res.end();
+   // Extension of file 
+   let extname = path.extname(filePath);
+
+   // Initial content type 
+   let contentType = 'text/html';
+
+   // Check ext and set content type 
+   switch (extname) {
+       case '.js':
+           contentType = 'text/javascript';
+           break;
+       case '.css':
+           contentType = 'text/css';
+           break;
+       case '.json':
+           contentType = 'application/json';
+           break;
+       case '.png':
+           contentType = 'image/png';
+           break;
+       case '.jpg':
+           contentType = 'image/jpg'
+           break;
+   }
+
+   // Read File
+   fs.readFile(filePath, (err, content) => {
+       if(err) {
+           if(err.code ==  'ENOENT') {
+               // Page not found
+               fs.readFile(path.join(__dirname, 'public', '404.html;'), (err, content) => {
+                   res.writeHead(200, {'Content-Tpe': 'text/html'});
+                   res.end(content, 'utf8');
+               })
+
+           }  else {
+               // Some Server error --> 500 
+               res.writeHead(500);
+               res.end(`Server error ${err.code}`);
+            }
+       } else {
+           // Success
+           res.writeHead(200, {'Content-Tpe': contentType});
+           res.end(content, 'utf8');
+       }
+   });
 });
 
 const PORT = process.env.PORT || 5000;
